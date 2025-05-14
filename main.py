2713ee2f27a64d3e809104391e9304dd43ca890d
lyasyun4ik <uliana.zaspa@hotmail.com>
2025-05-14 13:48:41 +0200

    add player movement

--- main.py
+++ main.py
@@ -20,13 +20,17 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        
+        # limit the framerate to 60 fps
+        dt = clock.tick(60) / 1000
+
+        # update player with fresh dt
+        player.update(dt)
+
+        # draw everything
         screen.fill((0,0,0))
         player.draw(screen)
         pygame.display.flip()
-
-        # limit the framerate to 60 fps
-        clock.tick(60)
-        dt = clock.tick(60) / 1000
         
 
 if __name__ == "__main__":
--- player.py
+++ player.py
@@ -7,7 +7,6 @@ class Player(CircleShape):
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
     
-    # in the player class
     def triangle(self):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
         right = pygame.Vector2(0, 1).rotate(self.rotation + 90) * self.radius / 1.5
@@ -18,3 +17,25 @@ class Player(CircleShape):
     
     def draw(self, screen):
         pygame.draw.polygon(screen, "white", self.triangle(), 2)
+
+    def rotate(self, dt):
+        self.rotation += PLAYER_TURN_SPEED * dt
+    
+    def move(self, dt):
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        self.position += forward * PLAYER_SPEED * dt
+    
+    def update(self, dt):
+        keys = pygame.key.get_pressed()
+
+        # rotate when pressing a or d
+        if keys[pygame.K_a]:
+            self.rotate(-dt)
+        if keys[pygame.K_d]:
+            self.rotate(dt)
+        
+        # move when pressing w or s
+        if keys[pygame.K_w]:
+            self.move(dt)
+        if keys[pygame.K_s]:
+            self.move(-dt)
